c326f68fe73409e12fe08eb0962b75b0d9ecd050
Dmytro Kovalenko <dkovalenko@4tifier.com>
2018-09-04 16:57:11 +0300

    works multi rooms but not pretty

--- src/main/java/com/dkovaleko/websocketchat/controller/chat/ChatRoomController.java
+++ src/main/java/com/dkovaleko/websocketchat/controller/chat/ChatRoomController.java
@@ -4,6 +4,7 @@ import com.dkovaleko.websocketchat.dto.chat.ChatRoom;
 import com.dkovaleko.websocketchat.service.chat.ChatRoomService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -26,6 +27,16 @@ public class ChatRoomController {
         ModelAndView view = new ModelAndView();
 
         chatRoomService.save(chatRoom);
+
+        view.setViewName("redirect:/users/" + userID + "/chat" + "");
+
+        return view;
+    }
+
+    @GetMapping(value = "/users/{userID}/chat/rooms/change")
+    public ModelAndView changeRoom(@PathVariable(value = "userID") long userID) {
+
+        ModelAndView view = new ModelAndView();
 
         view.setViewName("redirect:/users/" + userID + "/chat" + "");
 
